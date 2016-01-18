cd5670ece6e34214cb938659799eaf22c12de182
daou <daou@daou-3441>
2016-01-18 13:13:12 +0900

    123123123

--- git_test/src/git_test/test2.java
+++ git_test/src/git_test/test2.java
@@ -3,6 +3,6 @@ package git_test;
 public class test2 {
 	public static void main(String[] args){
 		System.out.println("test2");
-		System.out.println("주희야 이거 니가 수정하던가 덧붙여서 commit and push해봐라");
+		System.out.println("123123123123");
 	}
 }
