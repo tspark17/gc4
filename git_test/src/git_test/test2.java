5ce87417d550026eca3b896c17eb0e77fd0b28b0
tspark <tspark17@nate.com>
2016-01-18 13:08:39 +0900

    주희야 이거 해라

---
+++ git_test/src/git_test/test2.java
@@ -0,0 +1,8 @@
+package git_test;
+
+public class test2 {
+	public static void main(String[] args){
+		System.out.println("test2");
+		System.out.println("주희야 이거 니가 수정하던가 덧붙여서 commit and push해봐라");
+	}
+}
