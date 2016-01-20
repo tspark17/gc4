a1d94a3af0b85ad6a8f871e7b7cfb1f1ccb31bdb
daou <daou@daou-2910>
2016-01-20 13:57:45 +0900

    dd

--- git_test/src/git_test/test2.java
+++ git_test/src/git_test/test2.java
@@ -2,7 +2,7 @@ package git_test;
 
 public class test2 {
 	public static void main(String[] args){
-		System.out.println("test2");
-		System.out.println("TashinZZANG");
+		System.out.println("Fucking");
+	
 	}
 }
