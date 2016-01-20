e1e3e507609b19b18c637fb70621581836d39596
tspark <tspark17@nate.com>
2016-01-20 15:31:02 +0900

    test 3 30

--- git_test/src/git_test/test2.java
+++ git_test/src/git_test/test2.java
@@ -3,6 +3,12 @@ package git_test;
 public class test2 {
 	public static void main(String[] args){
 		System.out.println("Fucking");
-	
+		System.out.println("hi");
+		
+	test2 a = new test2();
+	a.test();
+	}
+	public void test(){
+		System.out.println("it's test method");
 	}
 }
