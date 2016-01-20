4fd1a6c171098286a254a498f34e76f4c3a523df
daou <daou@daou-2910>
2016-01-20 13:36:06 +0900

    ds

--- git_test/src/git_test/test2.java
+++ git_test/src/git_test/test2.java
@@ -3,6 +3,6 @@ package git_test;
 public class test2 {
 	public static void main(String[] args){
 		System.out.println("test2");
-		System.out.println("123123123123");
+		System.out.println("TashinZZANG");
 	}
 }
